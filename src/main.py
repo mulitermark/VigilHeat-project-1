69ed2fb8d6c08d7c9e6d55c0b9c07338ca6401c6
Rúzsa Ákos <fr2wt6@inf.elte.hu>
2023-05-16 10:05:29 +0200

    uncommented code

--- src/main.py
+++ src/main.py
@@ -3,18 +3,20 @@ import cv2
 from person_detector import PersonDetector
 from heatmap_generator import HeatmapGenerator
 
+
 def main():
     # Set video input path
     video_path = 'test.mp4'
 
-    # person_detector = PersonDetector(video_path)
-    # person_detector.detect()
-    # person_detector.save_detections("detections.npy")
+    person_detector = PersonDetector(video_path)
+    person_detector.detect()
+    person_detector.save_detections("detections.npy")
 
     heatmap_generator = HeatmapGenerator("detections.npy", 1270, 720)
     heatmap_generator.create_heatmap(0, 3000, 1)
 
     return 0
+
 
 if __name__ == "__main__":
     main()
