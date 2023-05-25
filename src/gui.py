3d116dea0f74fe96f6968b1e27309f01fbd25421
Rúzsa Ákos <fr2wt6@inf.elte.hu>
2023-05-25 18:00:12 +0200

    GUI update

--- src/gui.py
+++ src/gui.py
@@ -1,20 +1,27 @@
 import datetime
 import cv2
 import numpy as np
+import time
 
 from person_detector import PersonDetector
 from heatmap_generator import HeatmapGenerator
 from stream_handler import VideoStreamHandler
 from histogram_generator import HistogramGenerator
 
+FPS = 30
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+CAM_WIDTH = 1280
+CAM_HEIGHT = 720
+
 class Application:
-    def __init__(self, height=600, width=800, channels=3):
+    def __init__(self):
         self.updating = True
         self.frame = None
         self.heatmap = None
         self.histogram = None
 
-        self.heatmap_generator = HeatmapGenerator(1270, 720)
+        self.heatmap_generator = HeatmapGenerator(CAM_WIDTH, CAM_HEIGHT)
         self.histogram_generator = HistogramGenerator("minute")
 
         video_path = 'test.mp4'
@@ -22,19 +29,25 @@ class Application:
         self.person_detector = PersonDetector()
 
         # Create an empty canvas
-        self.canvas = np.zeros((height, width, channels), dtype=np.uint8)
+        self.canvas = np.ones((WINDOW_HEIGHT, WINDOW_WIDTH, 3), dtype=np.uint8) * 255
+        text_pos_x = WINDOW_WIDTH - 220
+        text_pos_y = WINDOW_HEIGHT - 120
+        cv2.putText(self.canvas, "CONTROLS:", (text_pos_x, text_pos_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+        cv2.putText(self.canvas, "Esc: Exit application", (text_pos_x, text_pos_y + 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+        cv2.putText(self.canvas, "P: Pause application", (text_pos_x, text_pos_y + 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+        cv2.putText(self.canvas, "R: Resume application", (text_pos_x, text_pos_y + 75), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
 
         cv2.namedWindow('Camera', cv2.WINDOW_NORMAL)
-        cv2.resizeWindow('Camera', width, height)
+        cv2.resizeWindow('Camera', WINDOW_WIDTH, WINDOW_HEIGHT)
 
         self.frame_num = 0
 
-    def update_canvas(self, canvas, frame, heatmap, histogram, height=600, width=800):
+    def update_canvas(self, canvas, frame, heatmap, histogram):
         # Calculate dimensions
-        cam_width = max(int(width * 0.6), 1)
-        cam_height = height
-        graph_width = max(int(width * 0.4), 1)
-        graph_height = max(int(height / 2), 1)
+        cam_width = int(WINDOW_WIDTH * 0.49)
+        cam_height = int(CAM_HEIGHT * cam_width / CAM_WIDTH)
+        graph_width = cam_width
+        graph_height = WINDOW_HEIGHT - cam_height
 
         # Check if frame, heatmap, histogram are not None and are numpy arrays
         if frame is not None and isinstance(frame, np.ndarray) and frame.size > 0:
@@ -44,36 +57,28 @@ class Application:
 
         if heatmap is not None and isinstance(heatmap, np.ndarray) and heatmap.size > 0:
             # Resize the image and update the canvas
-            heatmap_resized = cv2.resize(heatmap, (graph_width, graph_height))
-            canvas[:graph_height, cam_width:, 2] = heatmap_resized
+            heatmap_resized = cv2.resize(heatmap, (cam_width, cam_height))
+            canvas[:cam_height, -cam_width:] = heatmap_resized
 
         if histogram is not None and isinstance(histogram, np.ndarray) and histogram.size > 0:
             # Resize the image and update the canvas
             histogram_resized = cv2.resize(histogram, (graph_width, graph_height))
-            canvas[graph_height:, cam_width:] = histogram_resized
+            canvas[-graph_height:, :graph_width] = histogram_resized
 
         cv2.imshow('Camera', canvas)
 
         return canvas
 
-    def run(self, height=600, width=800):
+    def run(self):
         # Define video output parameters
         output_filename = 'output_video.mp4'
         codec = cv2.VideoWriter_fourcc(*'mp4v')
-        fps = 30
 
         # Initialize the VideoWriter
-        video_writer = cv2.VideoWriter(output_filename, codec, fps, (width, height))
+        video_writer = cv2.VideoWriter(output_filename, codec, FPS, (WINDOW_WIDTH, WINDOW_HEIGHT))
 
         while True:
-            key = cv2.waitKey(1) & 0xFF
-            if key == ord('p'):  # Pause the application when 'p' is pressed
-                self.updating = False
-            elif key == ord('r'):  # Resume the application when 'r' is pressed
-                self.updating = True
-            elif key == 27:  # Exit the application when 'ESC' is pressed
-                break
-
+            start_time = time.time()
             print(f"Processing frame {self.frame_num}...")
             current_time = datetime.datetime.now()
             if self.updating:
@@ -88,20 +93,38 @@ class Application:
                 # Write the canvas to the video file
                 video_writer.write(self.canvas)
 
+            current_time = datetime.datetime.now()
+            end_time = time.time()
+            elapsed = end_time - start_time
+            remaining = 1 / FPS - elapsed
+            if remaining > 0:
+                key = cv2.waitKey(int(remaining * 100)) & 0xFF
+            else:
+                key = cv2.waitKey(1) & 0xFF
+            if key == ord('p') or key == ord('P'):  # Pause the application when 'p' is pressed
+                self.updating = False
+            elif key == ord('r') or key == ord('R'):  # Resume the application when 'r' is pressed
+                self.updating = True
+            elif key == 27:  # Exit the application when 'ESC' is pressed
+                break
+
         # Release the VideoWriter and close the windows
         video_writer.release()
         cv2.destroyAllWindows()
 
-    def process_frame(self,current_time ):
+    def process_frame(self, current_time):
         # Increment frame number
         self.frame_num += 1
 
         # Read the next frame from the video
-        self.frame = self.streamer.get_frame()
-        if self.frame is None:
+        frame = self.streamer.get_frame()
+        if frame is None:
+            self.frame = None
             return
 
-        self.frame, detections = self.person_detector.detect(self.frame, self.frame_num)
-        self.heatmap = self.heatmap_generator.create_heatmap(detections, self.frame_num, 1)
+        self.frame, detections = self.person_detector.detect(frame.copy(), self.frame_num)
+        heatmap = self.heatmap_generator.create_heatmap(detections, self.frame_num, 1)
+        self.heatmap = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        self.heatmap[:, :, 2] = (self.heatmap[:, :, 2].astype(np.uint16) + heatmap.astype(np.uint16)).clip(0, 255).astype(np.uint8)
 
         self.histogram = self.histogram_generator.add_input(detections, current_time)
